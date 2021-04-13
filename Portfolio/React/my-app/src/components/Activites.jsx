6d7bdce0c958ad95edbcc1cec38e52bc739a365b
Tom <HE201667@students.ephec.be>
2021-04-13 17:03:08 +0200

    Portfolio

--- Portfolio/React/my-app/src/components/Activites.jsx
+++ Portfolio/React/my-app/src/components/Activites.jsx
@@ -12,45 +12,61 @@ function Activites() {
             <h1>Activités</h1>
             <p>Voici la liste des activités réalisées en dehors du cadre des cours de l'EPHEC TI</p>
             <div class="view view-first">
-                <img src={rn} alt="Réseau et sécurité"/>
+                <img src={rn} alt="Formation React Native"/>
                     <div class="mask">
                         <h2>Formation React Native</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <p>J'ai réalisé une formation React Native sur le site Open Classroom. Cette formation m'a aidé à
+                            développer une application dans la cadre du cours d'intégration. La formation a duré une trentaines d'heures.
+                        </p>
                     </div>
             </div>
             <div class="view view-first">
-                    <img src={ai} alt="Electronique"/>
+                    <img src={ai} alt="Formation IA et accessibilité"/>
                     <div class="mask">                  
-                        <h2>Formation IA</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <h2>Formation IA et accessibilité</h2>
+                        <p>Au début de mon stage, j'ai pris l'initiative de suivre une formation d'introduction à l'IA qui est un domaine qui
+                            m'était complètement flou. J'ai aussi suivi une formation sur l'accessibilité numérique suite à la séance d'information
+                            que nous avons reçu lors des cours à l'Ephec.
+                        </p>
                     </div>
             </div>
                 <div class="view view-first">
-                    <img src={csc} alt="Programmation"/>
+                    <img src={csc} alt="CSC"/>
                     <div class="mask">         
                         <h2>Cyber Security Challenge</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <p>J'ai en 2020 et 2021 participé à l'édition du CSC. Un jeu virtuel basé sur la sécurité qui consiste à retrouver des flags
+                            de différentes manières dans différents domaines. Ce challenge très complexe m'a beaucoup amusé et m'a beaucoup appris.
+                        </p>
                 </div>
                 </div>
                 <div class="view view-first">
-                    <img src={bmc} alt="IA" />
+                    <img src={bmc} alt="BMC" />
                     <div class="mask">
                         <h2>Brussel Management Challenge</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <p>Le 19 et 20 mars, nous avons dans une équipe de 5, participé au BMC. 3 cases nous ont été soumises et nous avions 4h pour
+                            trouver une solution à chacune d'elle. Cette expérience très différente de ce que nous faisons en informatique m'a appris
+                            beaucoup de choses que je n'ai jamais eu l'occasion de rencontrer.
+                        </p>
                     </div>
                 </div>
                 <div class="view view-first">
-                    <img src={ral} alt="IA" />
+                    <img src={ral} alt="Ralfagram" />
                     <div class="mask">
                         <h2>Ralfagram</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <p>Nous avons avec un autre étudiant d'IT à la suite du projet de développement web continué ce projet. Le photographe a souhaité
+                            nous engager pour continuer son site en dehors du cadre du cours. Nous avons travaillé une soixantaine d'heure supplémentaire pour
+                            rendre ce site totalement utilisable.
+                        </p>
                     </div>
                 </div>
                 <div class="view view-first">
-                    <img src={scout} alt="IA" />
+                    <img src={scout} alt="Scouts" />
                     <div class="mask">
                         <h2>Scouts</h2>
-                        <p>A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.</p>
+                        <p>Les scouts ont toujours fait partie de ma vie. J'ai commencé les baladins en 2005 à Brussel avant de rejoindre la 22ème BW en
+                            2007. Je n'y ai pas bougé depuis, je suis chef louveteaux depuis 3 ans ou j'organise : jeu, journée, week-end, camp pour 40 enfants
+                            de 8 à 12 ans.
+                        </p>
                     </div>
                 </div>
                                      
--- Portfolio/React/my-app/src/components/Resume.jsx
+++ Portfolio/React/my-app/src/components/Resume.jsx
@@ -4,6 +4,30 @@ function Resume() {
     return (
         <div>
             <h1>Resumé</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Thème</th>
+                        <th>Type d'activité</th>
+                        <th>Lieu</th>
+                        <th>Dates</th>
+                        <th>h valorisées</th>
+                        <th>h réelles</th>
+                        <th>lien vers la preuve de participation</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Formation</td>
+                        <td>Formation en React Native</td>
+                        <td>"Open Classroom"</td>
+                        <td>Du 30 septembre au 15 novembre</td>
+                        <td>10h</td>
+                        <td>30h</td>
+                        <td>Lien dispo <a><i>ici</i></a></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
     
